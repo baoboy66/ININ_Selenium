500627d6662e259ecf3ef1523634e3e175bca0c4
Administrator <Administrator@qfun.com>
2016-05-04 14:17:06 -0400

    code edited for review

--- TS2047/TC22038.cs
+++ TS2047/TC22038.cs
@@ -33,12 +33,16 @@
                         #region Pre Run Setup
                         using (Trace.TestCase.scope("Pre Run Setup"))
                         {
-                            // make sure the user is added to the right role.
-                            Users.SetRole(Rm.Users[0], _DEFAULT_ROLE);
-                            Status.Set(Rm.Users[0], "Available");
+                            TraceTrue(() =>
+                            {
+                                // make sure the user is added to the right role.
+                                Users.SetRole(Rm.Users[0], _DEFAULT_ROLE);
+                                Status.Set(Rm.Users[0], "Available");
 
-                            // get a driver for the test.
-                            Drivers = WebDriverManager.Instance.AddDriver(3);
+                                // get drivers for the test.
+                                Drivers = WebDriverManager.Instance.AddDriver(3);
+                                return true;
+                            }, "Pre run setup failed.");
                         }
                         #endregion
 
@@ -46,9 +50,12 @@
                         using (Trace.TestCase.scope("Step 1: Open a web browser and navigate to the webclient webpage for your IC server. "))
                         {
                             // Step 1 Verify: Webclient homepage opens without error.
-                            WebDriverManager.Instance.SwitchBrowser(Drivers[0]);
-                            Logon.GoToLogon();
-                            TraceTrue(() => Logon.IsAtServerForm(), "Step 1 - Couldn't get to the server form.");
+                            TraceTrue(() =>
+                            {
+                                WebDriverManager.Instance.SwitchBrowser(Drivers[0]);
+                                Logon.GoToLogon();
+                                return Logon.IsAtServerForm();
+                            }, "Step 1 - Couldn't get to the server form.");
                         }
                         #endregion
 
@@ -56,12 +63,11 @@
                         using (Trace.TestCase.scope("Step 2: Logon to the ic server with a user. "))
                         {
                             // Step 2 Verify: User logs in to IC server through webclient.
-                            TraceTrue(() => UserLogonAndStatusSet(Rm.Users[0], Rm.Stations[0], Drivers[0]), "Step 2 - Did not Logon successfully");
                             TraceTrue(() =>
                             {
                                 WebDriverManager.Instance.SwitchBrowser(Drivers[0]);
-                                return WaitFor(() => Util.IsLoggedIn());
-                            }, "Step 2 - Default views not displayed. Client does not appear to be connected.");
+                                return UserLogon(Rm.Users[0], Rm.Stations[0]);
+                            }, "Step 2 - Did not Logon successfully");
                         }
                         #endregion
 
@@ -69,9 +75,12 @@
                         using (Trace.TestCase.scope("Step 3: In the web browser, duplicate the webclient tab to create a duplicate session. "))
                         {
                             // Step 3 Verify: Tab is duplicated and the user is asked to Logon again.
-                            WebDriverManager.Instance.SwitchBrowser(Drivers[1]);
-                            Logon.GoToLogon();
-                            TraceTrue(() => Logon.IsAtServerForm(), "Step 3 - Couldn't get to the server page.");
+                            TraceTrue(() =>
+                            {
+                                WebDriverManager.Instance.SwitchBrowser(Drivers[1]);
+                                Logon.GoToLogon();
+                                return Logon.IsAtServerForm();
+                            }, "Step 3 - Couldn't get to the server page.");
                         }
                         #endregion
 
@@ -79,7 +88,11 @@
                         using (Trace.TestCase.scope("Step 4: Log the user in a second time using the same computer, user and station."))
                         {
                             // Step 4 Verify: The user is logged in and both sessions are active.
-                            TraceTrue(() => UserLogonAndStatusSet(Rm.Users[0], Rm.Stations[0], Drivers[1]), "Did not Logon successfully the second time.");
+                            TraceTrue(() =>
+                            {
+                                WebDriverManager.Instance.SwitchBrowser(Drivers[1]);
+                                return UserLogon(Rm.Users[0], Rm.Stations[0]);
+                            }, "Did not Logon successfully the second time.");
                             TraceTrue(() =>
                             {
                                 WebDriverManager.Instance.SwitchBrowser(Drivers[0]);
@@ -92,10 +105,10 @@
                         using (Trace.TestCase.scope("Step 5: Close the duplicated tab. "))
                         {
                             // Step 5 Verify: Original session is still open and does not close. 
-                            WebDriverManager.Instance.SwitchBrowser(Drivers[1]);
-                            WebDriverManager.Instance.CurrentDriver.Quit();
                             TraceTrue(() =>
                             {
+                                WebDriverManager.Instance.SwitchBrowser(Drivers[1]);
+                                WebDriverManager.Instance.CurrentDriver.Quit();
                                 WebDriverManager.Instance.SwitchBrowser(Drivers[0]);
                                 return WaitFor(() => Util.IsLoggedIn());
                             }, "Step 5 - Default views not displayed. Client does not appear to be connected.");
@@ -106,9 +119,12 @@
                         using (Trace.TestCase.scope("Step 6: In the web browser, duplicate the webclient tab to create a duplicate session. "))
                         {
                             // Step 6 Verify: Tab is duplicated and the user is asked to Logon again.
-                            WebDriverManager.Instance.SwitchBrowser(Drivers[2]);
-                            Logon.GoToLogon();
-                            TraceTrue(() => Logon.IsAtAnyLogonForm(), "Step 6 - Couldn't get to the Logon page.");
+                            TraceTrue(() =>
+                            {
+                                WebDriverManager.Instance.SwitchBrowser(Drivers[2]);
+                                Logon.GoToLogon();
+                                return Logon.IsAtAnyLogonForm();
+                            }, "Step 6 - Couldn't get to the Logon page.");
                         }
                         #endregion
 
@@ -116,15 +132,17 @@
                         using (Trace.TestCase.scope("Step 7: Log the user in a second time using the same computer and user, but a different station."))
                         {
                             //Step 7 Verify: The user is logged in and the original session is disconnected.
-                            TraceTrue(() => UserLogonAndStatusSet(Rm.Users[0], Rm.Stations[1], Drivers[2]), "Step 7 - Did not Logon successfully");
-                            WebDriverManager.Instance.SwitchBrowser(Drivers[2]);
                             TraceTrue(() =>
                             {
                                 WebDriverManager.Instance.SwitchBrowser(Drivers[2]);
-                                return WaitFor(() => Util.IsLoggedIn());
-                            }, "Step 7 - Default views not displayed. Client does not appear to be connected.");
-                            WebDriverManager.Instance.SwitchBrowser(Drivers[0]);
-                            TraceTrue(() => Logoff.IsAtLogoff(), "User still logged into both stations at the same time");
+                                return UserLogon(Rm.Users[0], Rm.Stations[1]);
+                            }, "Step 7 - Did not Logon successfully");
+                            TraceTrue(() =>
+                            {
+                                WebDriverManager.Instance.SwitchBrowser(Drivers[0]);
+                                return Logoff.IsAtLogoff();
+                                
+                            }, "User still logged into both stations at the same time");
                         }
                         #endregion
 
